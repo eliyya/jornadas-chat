e2ae479a20ec2a5dc2e0311592f33f40beafa079
eliyya <tecno_eli@hotmail.com>
2024-11-02 22:08:41 -0600

    message props pased

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -6,8 +6,12 @@ import { Message } from './Message'
 import { useEffect } from 'react'
 import { authenticateSocket } from '@/lib/socket'
 import { getToken } from '@/actions/auth'
+import { GithubSuccesResponse } from '@/types'
 
-export function MessageList() {
+interface MessageListProps {
+    user: GithubSuccesResponse
+}
+export function MessageList({ user }: MessageListProps) {
     const messages = useStore($messages)
     useEffect(() => {
         const executeAsync = async () => {
@@ -19,17 +23,16 @@ export function MessageList() {
     return (
         <main className="flex h-full flex-col justify-end gap-2 py-4 pl-8">
             {messages.map((message, i) => {
-                // const mine = message.username === user.username
-                const mine = false
+                const mine = message.username === user.login
                 const showName =
                     !mine && messages[i - 1]?.username !== message.username
                 const showAvatar =
                     !mine && messages[i + 1]?.username !== message.username
                 return (
                     <Message
-                        showAvatar={true}
-                        showName={true}
-                        mine={false}
+                        showAvatar={showAvatar}
+                        showName={showName}
+                        mine={mine}
                         message={message}
                         key={message.id}
                     />
